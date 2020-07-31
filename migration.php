95f8678a126c26ede7b31fc07e9696a31e362141
priyanka-gupta5505 <43770513+priyanka-gupta5505@users.noreply.github.com>
2020-07-31 23:35:05 +0530

    initial

---
+++ migration.php
@@ -0,0 +1,35 @@
+<?php
+ 
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+ 
+class Sample extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema:: create('sample',function(Blueprint $table)
+        {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('address');
+            $table->timestamp();
+        });
+    }
+ 
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
